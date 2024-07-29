9295c968e8e1db4e857f9aae0e3ad20209658f55
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-29 09:45:21 -0300

    :truck: move UUID responsability to models

--- src/models/authors.go
+++ src/models/authors.go
@@ -1,13 +1,11 @@
 package models
 
-import "github.com/google/uuid"
-
 type AuthorsIn struct {
 	Name string `json:"name,omitempty" gorm:"type:varchar(255);column:name"`
 }
 
 type AuthorsOut struct {
-	ID uuid.UUID `json:"id,omitempty" gorm:"type:uuid;column:id"` 
+	UUIDHolder
 	AuthorsIn
 }
 
