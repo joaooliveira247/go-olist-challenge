e39d970c397d0f7fd145e2847cb3459347f20184
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-29 09:47:45 -0300

    :sparkles: add GenUUID method

--- src/models/models.go
+++ src/models/models.go
@@ -5,3 +5,7 @@ import "github.com/google/uuid"
 type UUIDHolder struct {
 	ID uuid.UUID `json:"id,omitempty" gorm:"type:uuid;column:id"`
 }
+
+func (holder *UUIDHolder) GenUUID() {
+	holder.ID = uuid.New()
+}
