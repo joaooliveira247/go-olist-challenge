5fbc22fbc7ed17fbc074f6252e1ea54a486e9e6d
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-28 21:17:04 -0300

    :sparkles: add GORM tags

--- src/models/authors.go
+++ src/models/authors.go
@@ -3,11 +3,10 @@ package models
 import "github.com/google/uuid"
 
 type AuthorsIn struct {
-	Name string `json:"name,omitempty"`
+	Name string `json:"name,omitempty" gorm:"type:varchar(255);column:name"`
 }
 
 type AuthorsOut struct {
-	ID uuid.UUID `json:"id,omitempty"`
+	ID uuid.UUID `json:"id,omitempty" gorm:"type:uuid;column:id"` 
 	AuthorsIn
 }
-
