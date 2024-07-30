cf2f1b8c3c251f2c6ee7458e9f2511b58b89cf06
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-30 13:47:16 -0300

    :sparkles: add Delete func

--- src/db/manage.go
+++ src/db/manage.go
@@ -12,3 +12,17 @@ func Create(db *gorm.DB) {
 		log.Fatal(err)
 	}
 }
+
+func Delete(db *gorm.DB) {
+	db.Exec(
+		`
+do $$ declare
+    r record;
+begin
+    for r in (select tablename from pg_tables where schemaname = 'public') loop
+        execute 'drop table if exists ' || quote_ident(r.tablename) || ' cascade';
+    end loop;
+end $$;
+		`,
+	)
+}
