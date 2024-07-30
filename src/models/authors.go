3e3de53d45a5206ac4f354e9907b2ec8b833bb1c
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-29 22:08:57 -0300

    :fire: removed unnecessary structs

--- src/models/authors.go
+++ src/models/authors.go
@@ -1,15 +1,7 @@
 package models
 
-type AuthorsIn struct {
-	Name string `json:"name,omitempty" gorm:"type:varchar(255);column:name"`
-}
-
-type AuthorsOut struct {
-	UUIDHolder
-	AuthorsIn
-}
-
 type Authors struct {
-	AuthorsOut
-	IDPK uint `gorm:"type:primaryKey;column:id_pk"`
+	Name string `json:"name,omitempty" gorm:"type:varchar(255);column:name"`
+	UUIDHolder
+	IDPK uint `json:"-" gorm:"type:primaryKey;column:id_pk"`
 }
