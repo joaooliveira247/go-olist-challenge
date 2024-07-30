af1582b16c1d163af2e000e52bbeb54371d18ea2
joaooliveira247 <joaooliveira247@gmail.com>
2024-07-30 14:46:37 -0300

    :sparkles: add InsertAuthor method

--- src/repositories/authors.go
+++ src/repositories/authors.go
@@ -1,6 +1,10 @@
 package repositories
 
-import "gorm.io/gorm"
+import (
+	"github.com/google/uuid"
+	"github.com/joaooliveira247/go-olist-challenge/src/models"
+	"gorm.io/gorm"
+)
 
 type Author struct {
 	db *gorm.DB
@@ -8,4 +12,14 @@ type Author struct {
 
 func NewAuthorRepository(db *gorm.DB) *Author {
 	return &Author{db}
+}
+
+func (repository *Author) InsertAuthor(author models.Authors) (uuid.UUID, error) {
+	tx := repository.db.Begin()
+	if err := tx.Create(&author).Error; err != nil {
+		tx.Rollback()
+		return uuid.UUID{}, err
+	}
+	tx.Commit()
+	return author.ID, nil
 }
