978240dc52255eb5fd6e7e3031baff409fb87e78
joaooliveira247 <joaooliveira247@gmail.com>
2024-08-01 19:17:50 -0300

    :bug: fix json tags

--- src/models/books.go
+++ src/models/books.go
@@ -5,9 +5,9 @@ import "errors"
 type Book struct {
 	UUIDHolder
 	IDPK uint `gorm:"primaryKey;column:id_pk" json:"-"`
-	Title string `gorm:"type:varchar(255);not null;column:title"`
-	Edition uint8 `gorm:"type:smallint;column:edition"`
-	PublicationYear uint16 `gorm:"type:smallint;column:publication_year"`
+	Title string `gorm:"type:varchar(255);not null;column:title" json:"title"`
+	Edition uint8 `gorm:"type:smallint;column:edition" json:"edition"`
+	PublicationYear uint16 `gorm:"type:smallint;column:publication_year" json:"publication_year"`
 	Authors []string `gorm:"-" json:"authors"`
 }
 
