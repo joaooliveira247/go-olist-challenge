0324f19e8498d2a55584dca372d633adf738ca88
joaooliveira247 <joaooliveira247@gmail.com>
2024-08-02 16:15:48 -0300

    :fire: remove GetAuthorByName method

--- src/repositories/authors.go
+++ src/repositories/authors.go
@@ -45,27 +45,6 @@ func (repository *Author) GetAuthorsByName(name string) ([]models.Authors, error
 	return authors, nil
 }
 
-func (repository *Author) GetAuthorByName(name string) (models.Authors, error) {
-	var author models.Authors
-
-	if err := repository.db.Where("name = ?", name).First(&author).Error; err != nil {
-		return models.Authors{}, err
-	}
-
-	if author.Name == "" {
-		author.Name = name
-		if err := author.Prepare(); err != nil {
-			return models.Authors{}, err
-		}
-		if _, err := repository.InsertAuthor(author); err != nil {
-			return models.Authors{}, err
-		}
-		return author, nil
-	}
-
-	return author, nil
-}
-
 func (repository *Author) DeleteAuthor(id uuid.UUID) error {
 	if err := repository.db.Delete(&models.Authors{}, "id = ?", id).Error; err != nil {
 		return err
