7a6df9860074cc85db35e1dc40d6e717c3eae396
joaooliveira247 <joaooliveira247@gmail.com>
2024-08-02 13:05:51 -0300

    :bug: fix references in FK

--- src/models/models.go
+++ src/models/models.go
@@ -6,6 +6,13 @@ type UUIDHolder struct {
 	ID uuid.UUID `json:"id,omitempty" gorm:"unique;not null;type:uuid;column:id"`
 }
 
+type BookAuthors struct {
+	BookID   uint    `gorm:"primaryKey;column:book_id"`
+	Book     Book    `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:BookID;"`
+	AuthorID uint `gorm:"primaryKey;column:author_id"`
+	Author   Authors `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:AuthorID;"`
+}
+
 func (holder *UUIDHolder) GenUUID() {
 	holder.ID = uuid.New()
 }
