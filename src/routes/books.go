853fea7b3555f494e28b9db2865d61c4442540c7
joaooliveira247 <joaooliveira247@gmail.com>
2024-08-05 10:12:53 -0300

    :sparkles: add POST /book/ route

---
+++ src/routes/books.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joaooliveira247/go-olist-challenge/src/controllers"
+)
+
+func BooksRoutes(eng *gin.Engine) {
+	BooksRouter := eng.Group("/books")
+	{
+		BooksRouter.POST("/", controllers.CreateBook)
+	}
+}
