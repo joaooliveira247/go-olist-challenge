798c30eef9f3df14514e3e161161a994853e499c
joaooliveira247 <joaooliveira247@gmail.com>
2024-08-06 11:46:10 -0300

    :recycle: update Authors type

--- src/models/books.go
+++ src/models/books.go
@@ -3,16 +3,16 @@ package models
 import (
 	"errors"
 
-	"github.com/google/uuid"
+	"github.com/joaooliveira247/go-olist-challenge/src/utils"
 )
 
 type Book struct {
 	UUIDHolder
-	IDPK            uint        `gorm:"primaryKey;column:id_pk" json:"-"`
-	Title           string      `gorm:"type:varchar(255);not null;column:title" json:"title"`
-	Edition         uint8       `gorm:"type:smallint;column:edition" json:"edition"`
-	PublicationYear uint16      `gorm:"type:smallint;column:publication_year" json:"publication_year"`
-	Authors         []uuid.UUID `gorm:"-" json:"authors"`
+	IDPK            uint            `gorm:"primaryKey;column:id_pk" json:"-"`
+	Title           string          `gorm:"type:varchar(255);not null;column:title" json:"title"`
+	Edition         uint8           `gorm:"type:smallint;column:edition" json:"edition"`
+	PublicationYear uint16          `gorm:"type:smallint;column:publication_year" json:"publication_year"`
+	Authors         utils.UUIDArray `gorm:"column:authors" json:"authors"`
 }
 
 func (book *Book) validade() error {
