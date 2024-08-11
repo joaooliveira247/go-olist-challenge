9438045dcc18cd39370e8d7ba924ff2f13efe27b
joaooliveira247 <joaooliveira247@gmail.com>
2024-08-11 16:48:32 -0300

    :sparkles: add GetBooks

--- src/controllers/books.go
+++ src/controllers/books.go
@@ -7,6 +7,7 @@ import (
 	"github.com/joaooliveira247/go-olist-challenge/src/db"
 	"github.com/joaooliveira247/go-olist-challenge/src/models"
 	"github.com/joaooliveira247/go-olist-challenge/src/repositories"
+	"github.com/joaooliveira247/go-olist-challenge/src/utils"
 )
 
 func CreateBook(ctx *gin.Context) {
@@ -24,15 +25,50 @@ func CreateBook(ctx *gin.Context) {
 	db, err := db.GetDBConnection()
 
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"msg": "unexpected error"})
+		ctx.JSON(
+			http.StatusInternalServerError,
+			gin.H{"msg": "unexpected error"},
+		)
 		return
 	}
 
 	repository := repositories.NewBookRepository(db)
 
 	if _, err := repository.InsertBook(book); err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"msg": "unexpected error"})
+		ctx.JSON(
+			http.StatusInternalServerError,
+			gin.H{"msg": "unexpected error"},
+		)
 		return
 	}
 	ctx.JSON(http.StatusCreated, gin.H{"id": book.ID})
+}
+
+func GetBooks(ctx *gin.Context) {
+	db, err := db.GetDBConnection()
+
+	if err != nil {
+		ctx.JSON(
+			http.StatusInternalServerError,
+			gin.H{"msg": "unexpected error"},
+		)
+		return
+	}
+
+	repository := repositories.NewBookRepository(db)
+
+	var bookQuery utils.BookQuery
+	if ctx.ShouldBindQuery(&bookQuery) != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"msg": "invalid query"})
+		return
+	}
+	books, err := repository.GetBooks()
+	if err != nil {
+		ctx.JSON(
+			http.StatusInternalServerError,
+			gin.H{"msg": "unexpected error"},
+		)
+		return
+	}
+	ctx.JSON(http.StatusOK, books)
 }
