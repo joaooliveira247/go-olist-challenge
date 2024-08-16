a0142fddebbc29c675f7b289addd4f89390af582
joaooliveira247 <joaooliveira247@gmail.com>
2024-08-15 22:37:07 -0300

    :sparkles: add validate method

--- src/utils/query_params.go
+++ src/utils/query_params.go
@@ -1,11 +1,44 @@
 package utils
 
-import "github.com/google/uuid"
+import (
+	"reflect"
 
+	"github.com/google/uuid"
+)
 
 type BookQuery struct {
-	Title string `form:"title"`
-	PublicationYear uint16 `form:"publication_year"`
-	Edition uint8 `form:"edition"`
-	Author uuid.UUID `form:"author"`
+	Title           string    `form:"title"`
+	PublicationYear uint16    `form:"publication_year"`
+	Edition         uint8     `form:"edition"`
+	Author          uuid.UUID `form:"author"`
+	validParams     map[string]any
+}
+
+func (params *BookQuery) validate() {
+	nullRep := map[string]any{
+		"title":            "",
+		"publication_year": uint16(0),
+		"edition":          uint8(0),
+		"author":           uuid.Nil,
+	}
+	validMap := make(map[string]any)
+	val := reflect.Indirect(reflect.ValueOf(params))
+	typ := val.Type()
+
+	for i := 0; i < val.NumField(); i++ {
+		field := typ.Field(i)
+
+		if field.PkgPath != "" {
+			continue
+		}
+
+		key := field.Tag.Get("form")
+		value := val.Field(i).Interface()
+
+		if key != "" && value != nullRep[key] {
+			validMap[key] = value
+		}
+	}
+
+	params.validParams = validMap
 }
