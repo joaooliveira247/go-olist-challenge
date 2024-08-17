84f325dd10a721613c7ba8867a94869136087e57
joaooliveira247 <joaooliveira247@gmail.com>
2024-08-17 20:50:41 -0300

    :sparkles: add DeleteBook Controller

--- src/controllers/books.go
+++ src/controllers/books.go
@@ -2,9 +2,11 @@ package controllers
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/joaooliveira247/go-olist-challenge/src/db"
 	"github.com/joaooliveira247/go-olist-challenge/src/models"
 	"github.com/joaooliveira247/go-olist-challenge/src/repositories"
@@ -91,4 +93,44 @@ func GetBooks(ctx *gin.Context) {
 		return
 	}
 	ctx.JSON(http.StatusOK, books)
+}
+
+func DeleteBook(ctx *gin.Context) {
+	db, err := db.GetDBConnection()
+
+	if err != nil {
+		ctx.JSON(
+			http.StatusInternalServerError,
+			gin.H{"msg": "unexpected error"},
+		)
+		return
+	}
+
+	repository := repositories.NewBookRepository(db)
+
+	id, err := uuid.Parse(ctx.Param("id"))
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, "invalid id")
+		return
+	}
+
+	if err = repository.DeleteBook(id); err != nil {
+		if errors.Is(err, utils.BookNotFoundError) {
+			ctx.JSON(
+				http.StatusNotFound,
+				gin.H{
+					"msg": fmt.Sprintf("id %s not belongs any book", id),
+				},
+			)
+			return
+		}
+		ctx.JSON(
+			http.StatusInternalServerError,
+			gin.H{"msg": "unexpected error"},
+		)
+		return
+	}
+
+	ctx.JSON(http.StatusNoContent, nil)
 }
