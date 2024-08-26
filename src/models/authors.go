b5a0b77ae608a8df81a45783a7c4b2513293f01a
joaooliveira247 <joaooliveira247@gmail.com>
2024-08-26 11:38:55 -0300

    :sparkles: add ParseValidate method

--- src/models/authors.go
+++ src/models/authors.go
@@ -1,11 +1,15 @@
 package models
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/joaooliveira247/go-olist-challenge/src/utils"
+)
 
 type Authors struct {
 	Name string `json:"name,omitempty" gorm:"type:varchar(255);column:name"`
 	UUIDHolder
-	IDPK uint `json:"-" gorm:"primaryKey;column:id_pk"`
+	IDPK uint `json:"-"              gorm:"primaryKey;column:id_pk"`
 }
 
 func (author *Authors) validade() error {
@@ -20,5 +24,14 @@ func (author *Authors) Prepare() error {
 		return err
 	}
 	author.GenUUID()
+	return nil
+}
+
+func (author *Authors) ParseValidate(csv utils.AuthorCSV) error {
+	author.Name = csv.Name
+	if err := author.Prepare(); err != nil {
+		return err
+	}
+
 	return nil
 }
