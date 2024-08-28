183eef8879f74476f3fb8a0bfb6d875e5f8543e4
joaooliveira247 <joaooliveira247@gmail.com>
2024-08-28 10:36:04 -0300

    :sparkles: add main file

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/joaooliveira247/go-olist-challenge/src/cmd"
+)
+
 func main() {
-	
+	cli := cmd.Gen()
+
+	if err := cli.Run(os.Args); err !=nil {
+		log.Fatal(err)
+	}
 }
